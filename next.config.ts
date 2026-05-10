4b19f3cd7b6c438a32bec9a32a32008c6f8f7f83
aanasakhtar <anasakhtar851@gmail.com>
2026-05-10 22:33:15 +0500

    CICD pipeline fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+const isCI = process.env.CI === "true" || process.env.CI === "1";
+
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedDevOrigins: ["192.168.100.129"]
+  allowedDevOrigins: ["192.168.100.129"],
+  ...(isCI
+    ? {
+        experimental: {
+          cpus: 1,
+        },
+      }
+    : {}),
 };
 
 export default nextConfig;
